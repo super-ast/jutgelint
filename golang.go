b23101f5a0913dd478083327ce0b4eb075c68786
Daniel Martí <mvdan@mvdan.cc>
2015-06-23 01:03:34 +0200

    Add support for translating go to json

---
+++ golang.go
@@ -0,0 +1,27 @@
+/* Copyright (c) 2014-2015, Daniel Martí <mvdan@mvdan.cc> */
+/* See LICENSE for licensing information */
+
+package lintjutge
+
+import (
+	"encoding/json"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+
+	"github.com/mvdan/superast"
+)
+
+func encodeJsonFromGoCode(i io.Reader, filename string, w io.Writer) error {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, filename, i, 0)
+	if err != nil {
+		return err
+	}
+	a := superast.NewAST(fset)
+	ast.Walk(a, f)
+
+	enc := json.NewEncoder(w)
+	return enc.Encode(a.RootBlock)
+}
