71e6ed382aebd19e3f5d57edca5157a6371c57cd
Daniel Martí <mvdan@mvdan.cc>
2015-06-23 01:31:43 +0200

    Export golang func

--- golang.go
+++ golang.go
@@ -13,7 +13,7 @@ import (
 	"github.com/mvdan/superast"
 )
 
-func encodeJsonFromGoCode(i io.Reader, w io.Writer) error {
+func EncodeJsonFromGoCode(i io.Reader, w io.Writer) error {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "stdin.go", i, 0)
 	if err != nil {
