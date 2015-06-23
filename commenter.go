e47ea56d8e3db39232a8c74884bcdfdba9cf8cea
Daniel Martí <mvdan@mvdan.cc>
2015-06-23 17:12:48 +0200

    Lay out commenter.go

---
+++ commenter.go
@@ -0,0 +1,8 @@
+/* Copyright (c) 2014-2015, Daniel Martí <mvdan@mvdan.cc> */
+/* See LICENSE for licensing information */
+
+package jutgelint
+
+func CommentCode(warns []Warning, lines []string) []string {
+	return nil
+}
