266344b2c5d7efa77a5c540b89cf50da51b9c7d1
Daniel Martí <mvdan@mvdan.cc>
2015-06-25 13:56:50 +0200

    Implement CommentCode

--- commenter.go
+++ commenter.go
@@ -3,6 +3,27 @@
 
 package jutgelint
 
+import "bytes"
+
 func CommentCode(warns []Warning, lines []string) []string {
-	return nil
+	byLine := make([][]Warning, len(lines))
+	for _, w := range warns {
+		l := &byLine[w.Line]
+		*l = append(*l, w)
+	}
+	var comm []string
+	for i, l := range lines {
+		lineWarns := byLine[i]
+		b := bytes.NewBufferString(l)
+		b.WriteString(" // ")
+		for i, w := range lineWarns {
+			if i > 0 {
+				b.WriteString(", ")
+			}
+			b.WriteString(w.Long)
+		}
+		comm = append(comm, b.String())
+
+	}
+	return comm
 }
